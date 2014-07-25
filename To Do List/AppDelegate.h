d09ca5cd251a6546826281ca7c349bc0661c4e41
Aiden Yang <alyang250@gmail.com>
2014-07-25 15:19:35 -0700

    Initial Commit

---
+++ To Do List/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  To Do List
+//
+//  Created by Aiden Yang on 7/25/14.
+//  Copyright (c) 2014 aiden-yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
